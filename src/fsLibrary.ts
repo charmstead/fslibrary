5754d30284abc0d1918d98a9bdac5f9aec066905
tomide <tomideso@gmail.com>
2020-06-06 21:22:41 +0100

    refactor

--- src/fsLibrary.ts
+++ src/fsLibrary.ts
@@ -76,7 +76,7 @@ FsLibrary.prototype.getMasterCollection = function () {
 };
 
 FsLibrary.prototype.reinitializeWebflow = function () {
-  // (<any>window).Webflow.destroy();
+  (<any>window).Webflow.destroy();
   (<any>window).Webflow.ready();
   (<any>window).Webflow.require("ix2").init();
     initResize();
